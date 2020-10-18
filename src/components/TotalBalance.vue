59b2a9361d21526926590abf37eb533bc6a19ba0
Steve Jops <steven07kolesiko@gmail.com>
2020-10-18 20:05:47 +0300

    commit on "master"

---
+++ src/components/TotalBalance.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="total-value">BALANCE: {{ total }}</div>
+</template>
+
+<script>
+export default {
+  name: "TotalBalance",
+  props: {
+    total: {
+      type: Number,
+      default: 0,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.total-value {
+  font-size: 26;
+  text-transform: uppercase;
+  padding: 20px;
+  text-align: center;
+}
+</style>
